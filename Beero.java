9dde542e14153786bbb52c8e0d2c3c986ee7e6b4
DhirenSutha <348599382@gapps.yrdsb.ca>
2025-04-21 23:52:56 -0400

    add hero and dodge

--- Beero.java
+++ Beero.java
@@ -1,13 +1,14 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * After every mouse click Beero moves forward and turns 90 degrees to the right
+ * After every mouse click Beero moves to either the top or bottom and dodges
  * 
  * @Dhiren 
  * @April 16, 2025
  */
 public class Beero extends Actor
 {
+    boolean atTop = true;
     /**
      * Act - do whatever the Beero wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,8 +16,13 @@ public class Beero extends Actor
     public void act()
     {
         if(Greenfoot.mouseClicked(null)){
-            move(100);
-            turn(90);
+            atTop = !atTop;
+        }
+        
+        if(atTop) {
+            setLocation(100, 100);
+        } else {
+            setLocation(100, 300);
         }
     }
 }
